131d183c73ba5eb9786ad774b1edf3c729b15683
desmond1999d <desmond1999d@mail.ru>
2018-12-22 02:10:20 +0300

    car controls were added

---
+++ Assets/Nick/Scripts/SimpleMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour {
+
+    public float backWheelDistance = 1;
+    private GameObject dummyPivot;
+    float turningCenterDistance = 5;
+
+    // Use this for initialization
+    void Start () {
+        dummyPivot = new GameObject("dummyParent");
+        dummyPivot.transform.parent = this.transform;
+        dummyPivot.transform.localRotation = Quaternion.identity;
+        dummyPivot.transform.localPosition = Vector3.zero;
+        dummyPivot.transform.parent = null;
+        this.transform.parent = dummyPivot.transform;
+        this.transform.localPosition = new Vector3(0, 0, backWheelDistance);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * 2000f * Time.deltaTime, ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * 2000f * Time.deltaTime, ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 turningPivotPoint = dummyPivot.transform.TransformPoint(new Vector3(-turningCenterDistance, 0, 0));
+            dummyPivot.transform.RotateAround(turningPivotPoint, -Vector3.up, 20 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 turningPivotPoint = dummyPivot.transform.TransformPoint(new Vector3(turningCenterDistance, 0, 0));
+            dummyPivot.transform.RotateAround(turningPivotPoint, Vector3.up, 20 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 500f, ForceMode.Impulse);
+        }
+
+    }
+}
