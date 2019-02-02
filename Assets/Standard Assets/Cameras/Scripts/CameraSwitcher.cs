5745760021cb8d1ea05c80e4bc822f2671702fee
desmond1999d <desmond1999d@mail.ru>
2019-02-02 13:02:02 +0300

    Camera switching was added

---
+++ Assets/Standard Assets/Cameras/Scripts/CameraSwitcher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject fpsCamera;
+    [SerializeField]
+    private GameObject carCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        fpsCamera.SetActive(true);
+        carCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (fpsCamera.gameObject.active && !carCamera.gameObject.active)
+            {
+                fpsCamera.active = false;
+                carCamera.active = true;
+                Debug.Log("To car camera");
+            }
+            else if (!fpsCamera.gameObject.active && carCamera.gameObject.active)
+            {
+                fpsCamera.active = true;
+                carCamera.active = false;
+                Debug.Log("To fps camera");
+            }
+        }
+    }
+}
