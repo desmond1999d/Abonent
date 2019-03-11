92455e789427595e91fe3fb21b5e91f9c1f6f44b
Konstantin Sarambaev <konstantin.s@playgengary.com>
2019-03-11 20:20:39 +0300

    - added basic logic for connectors

---
+++ Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConnectionManager : SingletonMonoBehaviour<ConnectionManager>
+{
+    [SerializeField]
+    private LineRenderer connectorPrefab = null;
+
+
+    private List<LineRenderer> connectors = new List<LineRenderer>();
+
+
+    public void CreateConnection(Transform fromObj, Transform toObj)
+    {
+        LineRenderer connection = Instantiate(connectorPrefab, transform);
+        connection.positionCount = 2;
+        connection.SetPosition(0, fromObj.position);
+        connection.SetPosition(1, toObj.position);
+
+        connectors.Add(connection);
+    }
+
+
+    public void DestroyConnection(int index)
+    {
+        Destroy(connectors[index]);
+        connectors.RemoveAt(index);
+    }
+}
